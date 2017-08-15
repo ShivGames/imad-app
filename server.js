6b93c05a7e286cca951fc43c85af31d9286f7910
ShivGames <shivaraman1706@gmail.com>
2017-08-15 12:02:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -58,11 +58,6 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two', function (req, res) {
-  //var articleName = req.params.articleName; 
-  res.send(createTemplate(articles.articleTwo));
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
