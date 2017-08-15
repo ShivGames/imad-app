6fc4b79acc5ed8876152adb84304244697cfff7c
ShivGames <shivaraman1706@gmail.com>
2017-08-15 20:43:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
 console.log('Loaded!');
+
+function makeText() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 5; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
+function ChangeText() {
+    var text = makeText();
+}
