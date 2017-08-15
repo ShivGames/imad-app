5a2289d86a12151771959d668c38c6f48813ac84
ShivGames <shivaraman1706@gmail.com>
2017-08-15 20:52:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,6 +22,8 @@ var marginLeft = 0;
 function moveRight() {
     marginLeft += 5;
     img.style.marginLeft = marginLeft + "px";
+    
+    if (marginLeft > 1000) marginLeft = 0;
 }
 
 img.onclick = function() {
