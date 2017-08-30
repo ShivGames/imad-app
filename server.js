c41146c66b4ce3bb92d442a89378c999119f5349
ShivGames <shivaraman1706@gmail.com>
2017-08-30 17:30:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var config = {
     database: 'raghuramankal30',
     host: 'db.imad.hasura-app.io',
     port:'5432',
-    password: process.env.DB_PASSWORD
+    password: 'db-raghuramankal30-78765'
 };
 var articles = {
     'article-one': {
