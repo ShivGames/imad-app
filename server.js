03e5810d6d9aa215f68915f298ade513871fc1ee
ShivGames <shivaraman1706@gmail.com>
2017-08-30 17:32:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ function createTemplate(data) {
 
 var pool = new Pool(config);
 app.get('/test-db', function(req, res) {
-    pool.query('select * from test', function(err, result) {
+    pool.query('SELECT * FROM test', function(err, result) {
         if (err) {
             res.status(500).send(err.toString());
         } else {
